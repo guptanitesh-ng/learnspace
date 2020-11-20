3ceac446e6ec12853c212ca588d776ec36159b8e
guptanitesh-ng <guptanitesh.ng@gmail.com>
2020-11-20 15:50:11 +0530

    removed unused code and added URLs to the application in help file.

--- elastic-search/src/main/java/com/concepts/elasticsearch/ElasticSearchApplication.java
+++ elastic-search/src/main/java/com/concepts/elasticsearch/ElasticSearchApplication.java
@@ -2,12 +2,13 @@ package com.concepts.elasticsearch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class ElasticSearchApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ElasticSearchApplication.class, args);
+		ConfigurableApplicationContext run = SpringApplication.run(ElasticSearchApplication.class, args);
 	}
 
 }
